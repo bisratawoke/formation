3cc7c4f59cebca81d418e3f74c7e4722077645b1
bisratawoke <bisratawoke52@gmail.com>
2024-07-01 14:57:48 +0300

    Subject Line: created field creation container component

--- src/components/ShortAnswerFieldCreationForm.tsx
+++ src/components/ShortAnswerFieldCreationForm.tsx
@@ -11,62 +11,20 @@ import {
   Typography,
 } from "@mui/material";
 import { BodyText } from "./FormInputSelectionDrawerDefaultContent";
+import FieldCreationFormContainer from "./FieldCreationFormContainer";
 
 export default function ShortAnswerFieldCreationForm() {
   return (
-    <Stack
-      sx={{
-        height: "100%",
-        padding: "24px",
-        paddingTop: "40px",
-      }}
-    >
-      <Stack>
-        <Stack spacing={4}>
-          <Stack
-            direction="row"
-            justifyContent={"space-between"}
-            alignItems={"center"}
-          >
-            <Typography
-              sx={{
-                fontSize: "20px",
-                fontWeight: 700,
-                lineHeight: "23px",
-                color: "rgb(55, 53, 47)",
-              }}
-            >
-              Short answer
-            </Typography>
-            <Button endIcon={<ArrowForward />} variant="contained" size="small">
-              Insert
-            </Button>
-          </Stack>
-          <Stack spacing={1}>
-            <BodyText>Use this for short text answers. Add an</BodyText>
-            <BodyText>answer label or placeholder text </BodyText>
-            <BodyText>for guidance.</BodyText>
-          </Stack>
+    <FieldCreationFormContainer
+      title={" Short answer"}
+      helperTextContainer={
+        <Stack spacing={1}>
+          <BodyText>Use this for short text answers. Add an</BodyText>
+          <BodyText>answer label or placeholder text </BodyText>
+          <BodyText>for guidance.</BodyText>
         </Stack>
-      </Stack>
-      <Stack
-        spacing={4}
-        sx={{
-          marginTop: "48px",
-        }}
-      >
-        <Divider />
-        <Box alignItems="center">
-          <Chip
-            label="Example"
-            sx={{
-              fontSize: "12px",
-              fontWeight: 600,
-              color: "rgb(137, 136, 132)",
-              lineHeight: "12px",
-            }}
-          />
-        </Box>
+      }
+      exampleContainer={
         <Stack spacing={1}>
           <Typography
             sx={{
@@ -86,7 +44,7 @@ export default function ShortAnswerFieldCreationForm() {
             placeholder="First name"
           />
         </Stack>
-      </Stack>
-    </Stack>
+      }
+    />
   );
 }
