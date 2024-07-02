9d2dbf7652d656ba356fc166a34fd8cd538e4350
bisratawoke <bisratawoke52@gmail.com>
2024-07-02 10:24:22 +0300

    Subject line: added some basic theme to the site

---
+++ src/components/Theme.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f50057",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+});
